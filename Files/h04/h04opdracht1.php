ef4f15a5b6d69741b25262d07d2901026526ca40
Daan645 <daan.jacobs@student.rocvf.nl>
2020-03-26 14:47:03 +0100

    Hoofdstuk 4 Toegevoegd met alle bijbehorende opdrachten.

--- Files/h04/h04opdracht1.php
+++ Files/h04/h04opdracht1.php
@@ -1,23 +1,45 @@
 <?php
 
 
-echo  "<h2>Opdracht 1: celsius naar fahrenheit</h2>";
+echo "<h2>Opdracht 1: celsius naar fahrenheit</h2>";
 
 //celsius naar fahrenheit function
 
-    function celsiusNaarFahrenheit($celsius)
-    {
-        $fahrenheit = ($celsius * 1.8) + 32;
+function celsiusNaarFahrenheit($celsius)
+{
+    $fahrenheit = ($celsius * 1.8) + 32;
 
-        return $celsius." graden Celsius = ".$fahrenheit." Fahrenheit.";
-    }
+    return $celsius . " graden Celsius = " . $fahrenheit . " Fahrenheit.";
+}
 
 echo celsiusNaarFahrenheit(10);
 
-    echo "<h2>Opdracht 2</h2>";
+echo "<h2>Opdracht 2: deelbaar door 3</h2>";
 
-    function deelbaarDoor3 ($invoer) {
-        if ($invoer ){
+//deelbaar door 3 opdracht
+// als de uitkomst niets is is de boolean false en als de uitkomst 1 is is de boolean true
+function deelbaarDoor3($invoer)
+{
+    if ($invoer % 3 == 0) {
+        $invoer = true;
 
-        }
+    } else {
+        $invoer = false;
     }
+    return "invoer = " . $invoer;
+}
+
+echo deelbaarDoor3(4);
+
+echo "<h2>Opdracht 3 omgekeerde string</h2>";
+
+//opdracht string omkeren
+
+function stringOmkeren ($stringInvoer){
+
+    return strrev("$stringInvoer");
+
+}
+
+echo stringOmkeren("Deze string is omgekeerd.");
+
